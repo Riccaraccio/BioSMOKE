6124994023c5bd076a51a42565f51276209a21fa
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2025-11-05 11:39:40 +0100

    run script checks for the existance of the solver first

--- examples/TGA/run.sh
+++ examples/TGA/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [ ! -f ../../build/source/BioSMOKEpp ]; then
+    echo "Executable not found: Compile the project first"
+    exit 1
+fi
+
 ./../../build/source/BioSMOKEpp --input input.dic
